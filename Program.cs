b3a152243e845651781e7986fa1e14772bcedf38
kostya <kostya_pgs@mail>
2022-10-11 13:52:56 +0700

    2

--- Program.cs
+++ Program.cs
@@ -8,3 +8,33 @@ Clear();
 string[] array = AskArray();
 string[] result = FindLessThan(array, 3);
 WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] input, int n) {
+    string[] output = new string[CountLessThan(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CountLessThan(string[] input, int n) {
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] AskArray() {
+    Write("Вводим значение через пробел: ");
+    return ReadLine().Split(" ");
+}
